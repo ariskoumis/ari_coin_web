5955e26e365eda64f05d6765c2e8e709b6e4825c
Ari Koumis <arikoumis@Aris-MacBook-Pro.local>
2019-05-08 22:52:57 -0700

    Initial push of routing code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from './routes';
 import './index.css';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Routes />
+    </Router>, 
+    document.getElementById('root')
+);
